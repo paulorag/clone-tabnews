d1d22d0d4f57e2bae4c2b5f589471cf0f244ddb5
Paulo <devpaulorag@gmail.com>
2024-11-23 08:26:32 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para praticar a criação de um projeto do zero acompanhando o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>The emptiness machine🎸</h1>
+function Home() {
+  return <h1>The emptiness machine🎸</h1>;
 }
 
 export default Home;
