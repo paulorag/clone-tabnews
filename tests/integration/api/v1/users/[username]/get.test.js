debc644c51f51186f1c80ed85b85249b937b1912
Paulo Roberto <devpaulorag@gmail.com>
2026-04-18 15:13:36 -0300

    feat: apply `authorization.filterOutput()` to all endpoints

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -12,8 +12,6 @@ describe("GET /api/v1/users/[username]", () => {
     test("With exact case match", async () => {
       await orchestrator.createUser({
         username: "MesmoCase",
-        email: "mesmo.case@email.com",
-        password: "senha123",
       });
 
       const response2 = await fetch(
@@ -27,9 +25,7 @@ describe("GET /api/v1/users/[username]", () => {
       expect(response2Body).toEqual({
         id: response2Body.id,
         username: "MesmoCase",
-        email: "mesmo.case@email.com",
         features: ["read:activation_token"],
-        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
@@ -42,8 +38,6 @@ describe("GET /api/v1/users/[username]", () => {
     test("With exact mismatch", async () => {
       await orchestrator.createUser({
         username: "CaseDiferente",
-        email: "case.diferente@email.com",
-        password: "senha123",
       });
 
       const response2 = await fetch(
@@ -57,9 +51,7 @@ describe("GET /api/v1/users/[username]", () => {
       expect(response2Body).toEqual({
         id: response2Body.id,
         username: "CaseDiferente",
-        email: "case.diferente@email.com",
         features: ["read:activation_token"],
-        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
